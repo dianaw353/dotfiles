59fb86b1a490a97d80adff287677e94110f049e4
Diana Ward <diana.ward535@gmail.com>
2024-07-15 18:50:24 -0600

    added additional ependency & xdg script

---
+++ roles/hyprland/files/hypr/scripts/xdg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# -----------------------------------------------------
+sleep 1
+
+# kill all possible running xdg-desktop-portals
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-gnome
+killall -e xdg-desktop-portal-kde
+killall -e xdg-desktop-portal-lxqt
+killall -e xdg-desktop-portal-wlr
+killall -e xdg-desktop-portal-gtk
+killall -e xdg-desktop-portal
+sleep 1
+
+# start xdg-desktop-portal-hyprland
+/usr/lib/xdg-desktop-portal-hyprland &
+sleep 2
+
+# start xdg-desktop-portal
+/usr/lib/xdg-desktop-portal &
+sleep 1
