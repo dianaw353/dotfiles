134948247b82803e342fa477d43b2dc2b70a2f75
Diana Ward <diana.ward535@gmail.com>
2024-08-08 16:41:27 -0600

    new settings look

--- roles/ags/files/ags/widget/settings/SettingsDialog.ts
+++ roles/ags/files/ags/widget/settings/SettingsDialog.ts
@@ -5,35 +5,32 @@ import options from "options"
 
 const current = Variable(layout[0].attribute.name)
 
-const Header = () => Widget.CenterBox({
+const Header = () => Widget.Box({
     class_name: "header",
-    start_widget: Widget.Button({
-        class_name: "reset",
-        on_clicked: options.reset,
-        hpack: "start",
-        vpack: "start",
-        child: Widget.Icon(icons.ui.refresh),
-        tooltip_text: "Reset",
-    }),
-    center_widget: Widget.Box({
-        class_name: "pager horizontal",
-        children: layout.map(({ attribute: { name, icon } }) => Widget.Button({
-            xalign: 0,
-            class_name: current.bind().as(v => `${v === name ? "active" : ""}`),
-            on_clicked: () => current.value = name,
-            child: Widget.Box([
-                Widget.Icon(icon),
-                Widget.Label(name),
-            ]),
-        })),
-    }),
-    end_widget: Widget.Button({
-        class_name: "close",
-        hpack: "end",
-        vpack: "start",
-        child: Widget.Icon(icons.ui.close),
-        on_clicked: () => App.closeWindow("settings-dialog"),
-    }),
+    children: [
+        Widget.Button({
+            class_name: "reset",
+            on_clicked: options.reset,
+            hpack: "start",
+            vpack: "start",
+            child: Widget.Icon(icons.ui.refresh),
+            tooltip_text: "Reset",
+        }),
+    ],
+})
+
+const Sidebar = () => Widget.Box({
+    class_name: "sidebar",
+    vertical: true,
+    children: layout.map(({ attribute: { name, icon } }) => Widget.Button({
+        xalign: 0,
+        class_name: current.bind().as(v => `${v === name ? "active" : ""}`),
+        on_clicked: () => current.value = name,
+        child: Widget.Box([
+            Widget.Icon(icon),
+            Widget.Label(name),
+        ]),
+    })),
 })
 
 const PagesStack = () => Widget.Stack({
@@ -51,13 +48,24 @@ export default () => RegularWindow({
             win.hide()
             return true
         })
-        win.set_default_size(500, 600)
+        win.set_default_size(800, 600)
     },
     child: Widget.Box({
         vertical: true,
         children: [
             Header(),
-            PagesStack(),
+            Widget.Box({
+                vertical: false,
+                children: [
+                    Sidebar(),
+                    Widget.Box({
+                        vertical: true,
+                        children: [
+                            PagesStack(),
+                        ],
+                    }),
+                ],
+            }),
         ],
     }),
 })
