767c58467977aac8c1ca5a76bfebb576abb27d9a
Diana Ward <diana.ward535@gmail.com>
2024-09-01 18:13:46 -0600

    minor improvements to hyprshade search

--- roles/ags/files/ags/widget/hyprshade/hyprshade.ts
+++ roles/ags/files/ags/widget/hyprshade/hyprshade.ts
@@ -4,35 +4,54 @@ import icons from "lib/icons";
 function Hyprshade() {
     const handleAccept = ({ text }) => {
         if (text) {
-            console.log(`Search text: ${text}`);
+            
         }
         entry.get_child(0).text = "";
     };
 
     const handleChange = ({ text }) => {
-        console.log(`Search text changed: ${text}`);
+        
     };
 
-    const entry = Widget.Box({
-        vertical: true,
+    const entry = Widget.Entry({
         hexpand: true,
+        primary_icon_name: icons.ui.search,
+        on_accept: handleAccept,
+        on_change: handleChange,
+    });
+
+    function focus() {
+        entry.text = "";
+        entry.set_position(-1);
+        entry.select_region(0, -1);
+        entry.grab_focus();
+    }
+
+    const layout = Widget.Box({
+        vertical: true,
+        css: "padding: 1px; margin-top: 120px;",
+        className: "hyprshade",
+        setup: self => self.hook(App, (_, win, visible) => {
+            if (win !== "hyprshade")
+                return;
+
+            if (visible) {
+                focus();
+            }
+        }),
         children: [
-            Widget.Entry({
+            Widget.Box({
+                vertical: true,
                 hexpand: true,
-                primary_icon_name: icons.ui.search,
-                on_accept: handleAccept,
-                on_change: handleChange,
+                children: [
+                    entry,
+                    Widget.Separator(),
+                ],
             }),
-            Widget.Separator(),
         ],
     });
 
-    return Widget.Box({
-        vertical: true,
-        css: "padding: 1px; margin-top: 100pt;",
-        className: "hyprshade",
-        children: [entry],
-    });
+    return layout;
 }
 
 export default () => PopupWindow({
