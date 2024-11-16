23247a5705336b5fe50d8f7976f381c597bc1e13
Bahar Kurt <kurtbahartr@users.noreply.github.com>
2024-11-17 01:27:32 +0300

    Drop GTK_IM_MODULE as fcitx complains about it

--- roles/i18n/files/profile.d/im_quirks.sh
+++ roles/i18n/files/profile.d/im_quirks.sh
@@ -3,4 +3,3 @@ export XMODIFIERS=@im=fcitx
 export QT_IM_MODULE=fcitx
 # Wayland is the priority here since Hyprland is a Wayland compositor.
 export QT_IM_MODULES="wayland;fcitx;"
-export GTK_IM_MODULE=fcitx
