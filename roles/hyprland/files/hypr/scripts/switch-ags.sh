df783d0359fd5f783a5796a1f94f197b352d1821
Diana Ward <diana.ward535@gmail.com>
2024-11-16 21:28:11 -0700

    fix for ags switching

--- roles/hyprland/files/hypr/scripts/switch-ags.sh
+++ roles/hyprland/files/hypr/scripts/switch-ags.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-pgrep ags > /dev/null
-if [[ "$?" != "0" ]]; then
-  agsv1 -q; ags run ~/.config/agsv2/config.js
+
+# Check if ags is running
+if pgrep -x "ags" > /dev/null; then
+    # If ags is running, kill it and start agsv1
+    echo "Stopping ags and starting agsv1..."
+    ags quit -i js
+    agsv1 run &
+elif pgrep -x "agsv1" > /dev/null; then
+    # If agsv1 is running, kill it and start ags
+    echo "Stopping agsv1 and starting ags..."
+    agsv1 -q
+    ags run -d ~/.config/agsv2 &
 else
-  ags quit; agsv1
+    # If neither are running, start ags
+    echo "Neither ags nor agsv1 are running. Starting ags..."
+    #ags run -d ~/.config/agsv2 &
 fi
+
