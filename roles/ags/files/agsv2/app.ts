ab89b4cb6a5ebc8263df8184de6d424bfb80538d
Diana Ward <diana.ward535@gmail.com>
2024-11-18 23:08:30 -0700

    add notification widget

--- roles/ags/files/agsv2/app.ts
+++ roles/ags/files/agsv2/app.ts
@@ -1,13 +1,20 @@
-import { App } from "astal/gtk3"
-import style from "./theme/style.scss"
-import Bar from "./widget/bar/Bar"
+import { App } from "astal/gtk3";
+import style from "./theme/style.scss";
+import Bar from "./widget/bar/Bar";
+import NotificationPopups from "./widget/notifications/NotificationPopups";
 
 App.start({
     css: style,
     instanceName: "js",
     requestHandler(request, res) {
-        print(request)
-        res("ok")
+        print(request);
+        res("ok");
     },
-    main: () => App.get_monitors().map(Bar),
-})
+    main: () => {
+        const monitors = App.get_monitors();
+        const bars = monitors.map(Bar);
+        const notifications = monitors.map(NotificationPopups);
+        
+        return [...bars, ...notifications];
+    }
+});
