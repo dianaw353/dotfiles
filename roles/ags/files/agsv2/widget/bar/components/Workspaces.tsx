9da5c200210572dc21e0aa13749d3f1beef57e5b
Diana Ward <diana.ward535@gmail.com>
2024-11-24 17:10:58 -0700

    remove comments

--- roles/ags/files/agsv2/widget/bar/components/Workspaces.tsx
+++ roles/ags/files/agsv2/widget/bar/components/Workspaces.tsx
@@ -1,25 +1,3 @@
-// import { Variable, bind } from "astal"
-// import Hyprland from "gi://AstalHyprland"
-// import { Gtk } from "astal/gtk3"
-
-// export default function Workspaces() {
-//     const hypr = Hyprland.get_default()
-
-//     return <box className="Workspaces">
-//         {bind(hypr, "workspaces").as(wss => wss
-//             .sort((a, b) => a.id - b.id)
-//             .map(ws => (
-//                 <button
-//                     className={bind(hypr, "focusedWorkspace").as(fw =>
-//                         ws === fw ? "focused" : "")}
-//                     onClicked={() => ws.focus()}>
-//                     {ws.id}
-//                 </button>
-//             ))
-//         )}
-//     </box>
-// }
-
 import { Variable, bind } from "astal";
 import Hyprland from "gi://AstalHyprland";
 import { Gtk } from "astal/gtk3";
