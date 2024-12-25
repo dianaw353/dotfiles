cfb11eb52f00767c64ac513a12736f14f34e9f49
Diana <diana.ward535@gmail.com>
2024-12-25 12:15:34 -0700

    add battery, shutdown,logout to qs
    more on the way :3

--- roles/ags/files/agsv2/widget/quicksettings/QuickSettings.tsx
+++ roles/ags/files/agsv2/widget/quicksettings/QuickSettings.tsx
@@ -1,10 +1,34 @@
 import { App, Astal, Gdk, Gtk } from "astal/gtk3"
+import { execAsync } from "astal/process";
+import BatteryLevel from "../bar/components/BatteryLevel";
 
 function hide() {
   App.get_window("quicksettings")!.hide();
 }
 
+async function handleAction(action: string){
+  try {
+    let command: string;
+    switch (action) {
+      case "shutdown":
+        command = "shutdown now";
+        break;
+      case "logout":
+        command = "systemctl logout";
+        break;
+      default:
+        return;
+    }
+    await execAsync(command);
+  } catch (err) {
+  }
+}
+
 export default function QS() {
+  const actions = [
+    { action: "logout", icon: "system-log-out-symbolic" },
+    { action: "shutdown", icon: "system-shutdown-symbolic" },
+  ]
   return(
     <window
       name="quicksettings"
@@ -21,8 +45,24 @@ export default function QS() {
     >
       <box className="quicksettings-container"
         hexpand={false}
-        heightRequest={500} widthRequest={500} 
         comment="We'll be filling the contents of this thing later...">
+        <box orientation="horizontal" spacing={16} className="mini-qsbuttons">
+          <button valign={Gtk.Align.LEFT} vertical>
+            <BatteryLevel />
+          </button>
+          {actions.map(({ action, icon}) => (
+            <box valign={Gtk.Align.TOP} vertical className="mini-qsbuttons-master">
+              <button
+                className="action-mini-qsbutton"
+                onClicked={() => handleAction(action)}
+                valign={Gtk.Align.CENTER}
+                vertical
+              >
+              <icon className="action-qsicon" icon={icon} />
+              </button>
+            </box>
+          ))}
+          </box>
           <eventbox onClick={hide} />
       </box>
     </window>
