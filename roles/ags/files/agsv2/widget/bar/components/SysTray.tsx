0cadfef5553f36da591f6f6a3fb49e54768d98a2
Bahar KURT <kurtbahartr@users.noreply.github.com>
2024-12-29 00:55:29 +0300

    AGSv2: Fix the systray using the base example

--- roles/ags/files/agsv2/widget/bar/components/SysTray.tsx
+++ roles/ags/files/agsv2/widget/bar/components/SysTray.tsx
@@ -4,7 +4,7 @@ import { Gdk } from "astal/gtk3";
 import Tray from "gi://AstalTray";
 
 export default function SysTray() {
-    const tray = Tray.get_default();
+    const tray = Tray.get_default()
 
     return (
         <box className="SysTray">
@@ -21,25 +21,16 @@ export default function SysTray() {
 
                 return validItems.map(item => {
                     if (item.iconThemePath) App.add_icons(item.iconThemePath);
-
-                    const menu = item.create_menu();
-                    const gicon = bind(item, "gicon"); // gIcon is guaranteed valid here
+                    const gicon = bind(item, "gicon");
 
                     return (
-                        <button
+                        <menubutton
                             tooltipMarkup={bind(item, "tooltipMarkup")}
-                            onDestroy={() => menu?.destroy()}
-                            onClickRelease={self => {
-                                menu?.popup_at_widget(
-                                    self,
-                                    Gdk.Gravity.SOUTH,
-                                    Gdk.Gravity.NORTH,
-                                    null
-                                );
-                            }}
-                        >
-                            <icon gIcon={gicon} />
-                        </button>
+                            usePopover={false}
+                            actionGroup={bind(item, "action-group").as(ag => ["dbusmenu", ag])}
+                            menuModel={bind(item, "menu-model")}>
+                            <icon gicon={gicon} />
+                        </menubutton>
                     );
                 });
             })}
